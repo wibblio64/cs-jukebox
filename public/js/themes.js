921e6fb1736cf0562af54b66255e7e6282354988
wibblio <156273930+wibblio64@users.noreply.github.com>
2026-09-07 10:48:08 -0400

    added a light mode that's based on the current time

---
+++ public/js/themes.js
@@ -0,0 +1,11 @@
+function checkDarkMode() {
+    const date = new Date();
+    if (date.getHours() < 6 || date.getHours() >= 18) {
+        document.documentElement.className = "dark-theme";
+    } else {
+        document.documentElement.className = "";
+    }
+    setTimeout(checkDarkMode, 1000);
+}
+
+checkDarkMode();
